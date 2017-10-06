a1d46b90501fcf45dd847fcf9d03d0bcdac8be06
RaulSantosCalvo <raul.santos.calvo@gmail.com>
2017-10-06 09:48:51 +0200

    update

--- index.js
+++ index.js
@@ -1,76 +1,143 @@
 var path = require('path')
-  , geoip = require('geoip-lite')
-  , winston = require('winston');
+  , fs = require('fs')
 
-require('winston-daily-rotate-file');
+var mMapHandler;
+var process;
 
-var date = function() {
-    return '[' +
-        (new Date().toISOString().replace(/T/, ' ').// replace T with a space
-        replace(/\..+/, ''))     // delete the dot and everything after
-        + ']';
-};
+var MapHandler = function () {
+  var self = this;
 
-var logger = function (filename, level) {
-    return new (winston.transports.DailyRotateFile)({
-        createTree: true,
-        datePattern: './log/yyyy/MM/dd/',
-        filename: filename,
-        json: false,
-        level: level,
-        prepend: true,
-        timestamp: function () {
-            return date();
+  var mAppMap
+    , mTestMap;
+
+  self.handleMap = function(route, callback){
+    if(route && route!=null && route!='app'){
+      return dirMap(route);
+    }
+    else {
+      var processroute = require.main.filename.split('\\');
+      process = processroute[processroute.length-1];
+      if (process.includes('app')){
+        if (mAppMap==null) {
+          mAppMap = appMap(path.dirname(require.main.filename));
+          return mAppMap;
         }
-    });
-};
-
-var outLogger = new (winston.Logger)({ transports: [ logger('./out.log', 'info') ] });
-var debugLogger = new (winston.Logger)({ transports: [ logger('./debug.log', 'debug') ] });
-var errLogger = new (winston.Logger)({ transports: [ logger('./err.log', 'error') ] });
-var connLogger = new (winston.Logger)({ transports: [ logger('./conn.log', 'info') ] });
-
-module.exports.log = function(msg, obj) {
-    if (typeof msg === 'object') {
-        console.log(date() + ' ', msg);
-        outLogger.info(JSON.stringify(msg, null, 2));
+        else {
+          return mAppMap;
+        }
+      }
+      else if (process.includes('mocha') || process.includes('karma') || process.includes('jasmine')){
+        if (mTestMap==null) {
+          mTestMap = testMap();
+          return mTestMap;
+        }
+        else {
+          return mTestMap;
+        }
+      }
     }
-    else {
-        console.log(date() + ' ' + msg, obj ? obj : '');
-        outLogger.info(msg + (obj ? JSON.stringify(obj, null, 2) : ''));
-    }
-};
+  }
 
-module.exports.error = function(msg, obj) {
-    if (typeof msg === 'object') {
-        console.error(date() + ' ', msg);
-        errLogger.error(JSON.stringify(msg, null, 2));
-    }
-    else {
-        console.error(date() + ' ' + msg, obj ? obj : '');
-        errLogger.error(msg + ' ' + (obj ? JSON.stringify(obj, null, 2) : ''));
-    }
-};
+  self.test = function(){
+    return require.main.filename;
+  }
 
-module.exports.connection_log = function(req, msg) {
-    this.log("connection request")
-    if (req.headers) connLogger.info('Request Headers: ', prettyjson.render(req.headers));
-    var ip = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress ||
-        req.ip;
-    var geolocation = geoip.lookup(ip);
-    var str = {
-        hostname: req.hostname,
-        ip: ip,
-        location: geolocation ? {
-            country: geolocation.country,
-            city: geolocation.city,
-            coords: geolocation.ll
-        } : null
-    };
-    console.log(date() + msg ? msg : ' ', str);
-    connLogger.info(msg + (str ? JSON.stringify(str, null, 2) : ''));
-    return (str);
-};
+}
+
+var createMap = function(directory, callback){
+  var schema = {};
+  var dirMap = fs.readdirSync(directory);
+  dirMap.forEach(function(dir,i,a) {
+    var dirPath = path.join(directory, dir);
+    var stats = fs.lstatSync(dirPath);
+    if (stats.isDirectory()) {
+      createMap(dirPath, function(structure){
+        schema[dir]=structure;
+      });
+    } else {
+      var splits = dir.split('.');
+      var extension = splits[splits.length - 1];
+      if(extension=='js'){
+        var name = dir.split('.')[0];
+        schema[name]=dirPath;
+      }
+    }
+  });
+  callback(schema);
+}
+
+var appMap = function(root, callback){
+  var mMap = {};
+  //var root = path.dirname(require.main.filename);
+  createMap(path.join(root, 'app'), function(map){
+    mMap['app'] = map;
+  })
+  createMap(path.join(root, 'config'), function(map){
+    mMap['config'] = map;
+  })
+
+  if (callback) callback(mMap);
+  else return mMap;
+}
+
+var testMap = function(callback){
+  var basepath = path.join(__dirname, '../../server');
+  console.log("basepath: ", basepath);
+  return appMap(basepath);
+}
+
+var dirMap = function(root){
+  var mMap = {};
+  if(!root || root==null || !root.trim().length>0) {
+    return false
+  }
+  else {
+    createMap(root, function(map){
+      mMap = map;
+    })
+  }
+  return mMap;
+}
+
+var getElement = function(package, route){
+  var element = mMapHandler.handleMap().app[package];
+  route.split('/').forEach(function(e){
+    element = element[e];
+  });
+  return element;
+}
+
+var checkHandler = function(){
+  if (mMapHandler==null) {
+    mMapHandler = new MapHandler();
+  }
+}
+
+module.exports = {
+
+  map : function(order) {
+    checkHandler();
+    return mMapHandler.handleMap(order);
+  },
+
+  getConfig : function(element){
+    checkHandler();
+    return mMapHandler.handleMap().config[element];
+  },
+
+  getController : function(fullroute){
+    checkHandler();
+    return getElement('controllers', fullroute);
+  },
+
+  getService : function(fullroute){
+    checkHandler();
+    return getElement('services', fullroute);
+  },
+
+  getModel : function(fullroute){
+    checkHandler();
+    return getElement('models', fullroute);
+  }
+
+}
